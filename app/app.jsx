ed5c08359167bf081e941a90e3024022b74895d8
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-04-28 15:11:40 +0200

    Add index route for weather

--- app/app.jsx
+++ app/app.jsx
@@ -2,11 +2,12 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('app/components/Main.jsx');
+var Weather = require('app/components/Weather.jsx');
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
-
+      <IndexRoute component={Weather}/>
     </Route>
   </Router>,
   document.getElementById('app')
