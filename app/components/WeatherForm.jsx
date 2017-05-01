7fbd056206014513ab5d2a6354fda3c2f05e33eb
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-05-01 16:26:02 +0200

    Add render function to WeatherForm

---
+++ app/components/WeatherForm.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var WeatherForm = React.createClass({
+  render: function () {
+    return (
+      <h4>Weather Form<h4>
+    );
+  }
+});
+
+module.exports = WeatherForm;
