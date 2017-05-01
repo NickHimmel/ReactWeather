4c0cfbc5b9e418139ed11c16f8bed1439c0685e9
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-05-01 16:21:25 +0200

    Add active styles

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,14 +1,14 @@
 var React = require('react');
-var {Link} = require('react-router');
+var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
   render: function() {
       return (
         <div>
           <h2>Nav Component</h2>
-          <Link to="/" activeClassName="active">Get Weather</Link>
-          <Link to="/about" activeClassName="active">About</Link>
-          <Link to="/examples" activeClassName="active">Examples</Link>
+          <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+          <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+          <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
         </div>
 
       );
