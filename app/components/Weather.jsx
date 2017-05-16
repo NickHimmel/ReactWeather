1f6386deb32ffa31f5b9273d51dc72fa7e22e6b6
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-05-16 19:14:07 +0200

    Complete 35 Making our api call

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var WeatherForm = require('app/components/WeatherForm.jsx')
 var WeatherMessage = require('app/components/WeatherMessage.jsx')
+var openWeatherMap = require('app/api/openWeatherMap.jsx')
 
 var Weather = React.createClass({
   getInitialState: function () {
@@ -10,9 +11,15 @@ var Weather = React.createClass({
     }
   },
   handleSearch: function (location) {
-    this.setState({
-      location: location,
-      temp: 23
+    var that = this;
+
+    openWeatherMap.getTemp(location).then(function (temp) {
+      that.setState({
+        location: location,
+        temp: temp
+      });
+    }, function (errorMessage) {
+      alert(errorMessage);
     })
   },
   render: function () {
